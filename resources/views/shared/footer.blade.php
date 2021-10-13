9d231088226b473808d26c50530f9ac446c66d89
Wout Hebberecht <r0844999@student.thomasmore.be>
2021-10-13 16:41:02 +0200

    Einde les

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="container border-top pt-7 text-right">
+    <p>The vinyl shop - Year</p>
+</footer>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    @include('shared.incons')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title', 'The Vinyl Shop')</title>
 </head>
 <body>
@@ -15,7 +16,7 @@
     @yield('main', 'Page under construction ...')
 </main>
 {{--  Footer  --}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.js"></script>
+    @include('shared.footer')
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
