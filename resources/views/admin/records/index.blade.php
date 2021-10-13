13c2ff9f10c41807b34e4e6091f42bf2a540a7b2
Wout Hebberecht <r0844999@student.thomasmore.be>
2021-10-13 14:31:47 +0200

    first commit

---
+++ resources/views/admin/records/index.blade.php
@@ -0,0 +1,8 @@
+<ul>
+    <?php
+    foreach ($records as $record){
+        echo "<li> $record </li>";
+        //echo '<li>' . $record . '</li>'; This looks in the 'routes/web.php' for the variable
+    }
+    ?>
+</ul>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/* Lange manier -- Meerdere opties
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+Route::prefix('admin')->group(function () {
+    Route::redirect('/', 'records');
+    Route::get('records', 'Admin\RecordController@index'); // Looks in this file for the Arrays
+});
+
+// Route::prefix('admin')
+
+// Korte manier
+Route::view('/', 'home');
+Route::view('contact-us', 'contact');
+Route::view("home", "home");
+
